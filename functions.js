185060272c07bbf2fb57b0f71155dd15d8fed75d
Nathan Santos <santos.nna@outlook.com>
2022-11-15 18:17:24 +0100

    Update: Drafted new endpoints and created a simple test.

--- functions.js
+++ functions.js
@@ -2,19 +2,14 @@ const fs = require("fs");
 
 var file = "./books-data.json";
 
-module.exports = {
-  append_new_book_to_json_file: function (newBook) {
-    fs.readFile(file, "utf-8", (err, data) => {
-      let json = JSON.parse(data);
-      json.push(newBook);
-      fs.writeFile(file, JSON.stringify(json, null, 2), (err) => {
-        if (err) throw err;
-      });
-    });
-
-    fs.readFile(file, "utf-8", (err, data) => {
+function append_new_book_to_json_file(newBook) {
+  fs.readFile(file, "utf-8", (err, data) => {
+    let json = JSON.parse(data);
+    json.push(newBook);
+    fs.writeFile(file, JSON.stringify(json, null, 2), (err) => {
       if (err) throw err;
-      console.log(data);
     });
-  },
-};
+  });
+}
+
+module.exports = { append_new_book_to_json_file };
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { append_new_book_to_json_file } = require("./functions");
+var books = require("./books-data.json");
 
 const app = express();
-
-var books = require("./books-data.json");
+const { append_new_book_to_json_file } = require("./functions");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,14 +13,25 @@ app.get("/", (req, res) => {
 
 app.get("/:id", (req, res) => {
   let id = req.params.id;
-  var book = books.find((element) => element.id.$oid === id);
+  var book = books.find((element) => element.id === id);
   res.send(book);
 });
+
+app.get("/new", (req, res) => {}); // Will contain a form to add new registers
 
 app.post("/new", (req, res) => {
   let newBook = req.body;
   append_new_book_to_json_file(newBook);
   res.send(newBook);
+  // res.redirect("/" + newBook.id);
 });
+
+app.get("/update", (req, res) => {}); // Will contain a form to update fields
+
+app.put("/update", (req, res) => {}); // Will handle register update
+
+app.get("/delete", (req, res) => {}); // Will contain a page to certify the exclusion
+
+app.delete("/delete", (req, res) => {}); // Will handle register deletion
 
 app.listen(3000, () => console.log("Listening on port 3000"));
