013589a9e06a1a9777b1bbaa84c7e52ea86887b8
Nathan Santos <santos.nna@outlook.com>
2023-03-08 01:16:12 +0100

    Added seeds file.

---
+++ seed/index.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Book = require("../models/books");
+const booksList = require("./books-data.json");
+
+mongoose.connect("mongodb://localhost:27017/book-directory", {
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+	console.log("Database connected");
+});
+
+const seedDB = async () => {
+	try {
+		await Book.deleteMany({});
+		for (let i = 0; i < booksList.length; i++) {
+			const book = new Book(booksList[i]);
+			await book.save();
+		}
+	} catch (err) {
+		throw err;
+	}
+};
+
+seedDB().then(() => {
+	mongoose.connection.close();
+});
